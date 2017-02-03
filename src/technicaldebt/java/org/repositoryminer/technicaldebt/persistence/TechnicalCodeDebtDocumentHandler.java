48d50567484e2fedb80424312dc36b44c45a3639
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2017-02-03 11:32:03 -0300

    TD queries added.

--- src/technicaldebt/java/org/repositoryminer/technicaldebt/persistence/TechnicalCodeDebtDocumentHandler.java
+++ src/technicaldebt/java/org/repositoryminer/technicaldebt/persistence/TechnicalCodeDebtDocumentHandler.java
@@ -1,7 +1,15 @@
 package org.repositoryminer.technicaldebt.persistence;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.repositoryminer.persistence.Connection;
 import org.repositoryminer.persistence.handler.DocumentHandler;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.model.Filters;
 
 public class TechnicalCodeDebtDocumentHandler extends DocumentHandler {
 
@@ -10,5 +18,78 @@ public class TechnicalCodeDebtDocumentHandler extends DocumentHandler {
 	public TechnicalCodeDebtDocumentHandler() {
 		super.collection = Connection.getInstance().getCollection(COLLECTION_NAME);
 	}
-	
+
+	public List<Document> findByCommit(String commit) {
+		final Bson where = new BasicDBObject("commit", commit);
+		return findMany(where);
+	}
+
+	public List<Document> findByTypes(String commit, List<String> types) {
+		final List<Bson> clauses = new ArrayList<Bson>();
+
+		final Bson clause1 = new BasicDBObject("commit", commit);
+		clauses.add(clause1);
+
+		if (types != null && !types.isEmpty()) {
+			final Bson clause2 = Filters.in("debts", types);
+			clauses.add(clause2);
+		}
+
+		final Bson where = Filters.and(clauses);
+		return findMany(where);
+	}
+
+	public List<Document> findByIndicators(String commit, List<String> indicators) {
+		final List<Bson> clauses = new ArrayList<Bson>();
+
+		final Bson clause1 = new BasicDBObject("commit", commit);
+		clauses.add(clause1);
+
+		if (indicators != null && !indicators.isEmpty()) {
+			final List<Bson> existClauses = new ArrayList<Bson>();
+			for (String i : indicators) {
+				existClauses.add(Filters.exists("indicators."+i));
+			}
+			clauses.add(Filters.or(existClauses));
+		}
+
+		final Bson where = Filters.and(clauses);
+		return findMany(where);
+	}
+
+	public List<Document> findByConfirmation(String commit, boolean isTD) {
+		final Bson clause1 = new BasicDBObject("commit", commit);
+		final Bson clause2 = new BasicDBObject("technical_debt", isTD);
+		final Bson where = Filters.and(clause1, clause2);
+		return findMany(where);
+	}
+
+	public List<Document> findToManagement(String commit, List<String> types, List<String> indicators, Boolean isTD) {
+		final List<Bson> clauses = new ArrayList<Bson>();
+
+		final Bson clause1 = new BasicDBObject("commit", commit);
+		clauses.add(clause1);
+		
+		if (types != null && !types.isEmpty()) {
+			final Bson clause2 = Filters.in("debts", types);
+			clauses.add(clause2);
+		}
+		
+		if (indicators != null && !indicators.isEmpty()) {
+			final List<Bson> existClauses = new ArrayList<Bson>();
+			for (String i : indicators) {
+				existClauses.add(Filters.exists("indicators."+i));
+			}
+			clauses.add(Filters.or(existClauses));
+		}
+		
+		if (isTD != null) {
+			final Bson clause3 = new BasicDBObject("technical_debt", isTD);
+			clauses.add(clause3);
+		}
+		
+		final Bson where = Filters.and(clauses);
+		return findMany(where);
+	}
+
 }
